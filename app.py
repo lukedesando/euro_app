114a46912a232e36ff43760ebe15485cfe3c1f4d
lukedesando <Lukedesando@gmail.com>
2024-03-08 00:03:07 -0500

    Initial Flask Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+# Database connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="FuckYou123",
+    database="eurovision_db"
+)
+
+@app.route('/songs', methods=['GET'])
+def get_songs():
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM songs")
+    songs = cursor.fetchall()
+    cursor.close()
+    return jsonify(songs)
+
+@app.route('/vote', methods=['POST'])
+def vote():
+    data = request.json
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO votes (user_name, song_id, score) VALUES (%s, %s, %s)",
+                   (data['user_name'], data['song_id'], data['score']))
+    db.commit()
+    cursor.close()
+    return jsonify({"message": "Vote recorded"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
