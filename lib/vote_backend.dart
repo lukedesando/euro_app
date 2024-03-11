5155b9d6bfa6a2e08f12415ae95df0101bf84b68
lukedesando <Lukedesando@gmail.com>
2024-03-11 04:03:39 -0400

    Added Config references

--- lib/vote_backend.dart
+++ lib/vote_backend.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:dotenv/dotenv.dart' as dotenv;
+import 'config.dart';
 
-void main() {
-  dotenv.load(); // Load the .env file
-}
+String songsHTTP = 'http://${Config.DB_HOST}:5000/songs';
+String voteHTTP = 'http://${Config.DB_HOST}:5000/vote';
 
-String DB_HOST = dotenv.env['DB_HOST'] ?? 'localhost';
-// String songsHTTP = 'http://${DB_HOST}:5000/songs';
-const String songsHTTP = 'http://localhost:5000/songs';
-const String voteHTTP = 'http://localhost:5000/vote';
+// const String songsHTTP = 'http://localhost:5000/songs';
+// const String voteHTTP = 'http://localhost:5000/vote';
 
 class VoteWidget extends StatelessWidget {
   final Widget child;
