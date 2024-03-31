8839a3511180593700567ade5f7291456f35a006
lukedesando <Lukedesando@gmail.com>
2024-03-31 17:32:54 -0400

    Refactoring Changes

---
+++ lib/widgets/score_slider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../vote_backend.dart';
+
+class ScoreSlider extends StatefulWidget {
+  final Function(double) onScoreChanged;
+
+  const ScoreSlider({Key? key, required this.onScoreChanged}) : super(key: key);
+
+  @override
+  _ScoreSliderState createState() => _ScoreSliderState();
+}
+
+class _ScoreSliderState extends State<ScoreSlider> {
+  double _currentScore = 5.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: _currentScore,
+      min: 0,
+      max: 10,
+      divisions: 10,
+      label: _currentScore.round().toString(),
+      onChanged: (double value) {
+        setState(() {
+          _currentScore = value;
+        });
+        widget.onScoreChanged(value);
+      },
+    );
+  }
+}
