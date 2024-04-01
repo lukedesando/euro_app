85b48908d74fa27d009dfc80feb6c9a7adfb2639
lukedesando <Lukedesando@gmail.com>
2024-04-01 00:42:58 -0400

    Deleted unnessary code

--- lib/vote_backend.dart
+++ lib/vote_backend.dart
@@ -1,16 +1,4 @@
-import 'package:flutter/material.dart';
 import 'config.dart';
 
 String songsHTTP = 'http://${Config.DB_HOST}:${Config.DB_PORT}/songs';
 String voteHTTP = 'http://${Config.DB_HOST}:${Config.DB_PORT}/vote';
-
-class VoteWidget extends StatelessWidget {
-  final Widget child;
-  
-  const VoteWidget({Key? key, required this.child}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return child;
-  }
-}
