21ad15639e3ba758ee67191e22b0dfc0386f4f8e
lukedesando <Lukedesando@gmail.com>
2024-05-25 12:56:38 -0400

    Added confetti buffer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:euro_app/Global.dart';
 import 'package:euro_app/models/x_count_model.dart';
 import 'package:euro_app/services/socket_service.dart';
 import 'package:provider/provider.dart';
+import 'dart:async';
 
 import '../widgets/dropdowns/song_dropdown.dart';
 import '../widgets/sliders/score_slider.dart';
@@ -30,6 +31,7 @@ class _HomePageState extends State<HomePage> {
   String? selectedCountry;
   final TextEditingController nameController = TextEditingController();
   late final SocketService _socketService;
+  bool _showGlitter = false;
 
   @override
   void initState() {
@@ -77,11 +79,23 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _onVoteButtonPressed() {
+    setState(() {
+      _showGlitter = true; // Show glitter effect
+    });
+    // Hide glitter effect after 2 seconds
+    Timer(Duration(seconds: 4), () {
+      setState(() {
+        _showGlitter = false;
+      });
+    });
+
+    // Perform the vote update logic
     // Update the xCountModel after voting
     setState(() {
       // Trigger Consumer update
     });
   }
+
 
   void _onXButtonPressed() {
     // Update the xCountModel after voting
@@ -101,70 +115,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarEuro(title: 'Vote for Eurovision Song'),
-      body: GlitterBox(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              NameInputField(
-                controller: nameController,
-                onNameChanged: _onNameChanged,
-              ),
-              const SizedBox(height: 20),
-              const Text('Select Country:'),
-              SongDropdown(
-                onSongSelected: _onSongChanged,
-                songId: _selectedSongID ?? 0,
-                userName: currentUserName ?? '',
-                x_count: Global.xCountModel.xCount, // Use global x_count
-              ),
-              const SizedBox(height: 20),
-              ScoreSlider(onScoreChanged: _onScoreChanged),
-              const SizedBox(height: 20),
-              Center(
-                child: VoteButton(
-                  songName: selectedSong ?? 'No Song Selected',
-                  songId: _selectedSongID ?? 0,
-                  userName: nameController.text,
-                  score: _currentScore,
-                  country: selectedCountry ?? 'No Country Selected',
-                  onUpdate: _onVoteButtonPressed,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: XButton(
-                  songName: selectedSong ?? 'No Song Selected',
-                  songId: _selectedSongID ?? 0,
-                  userName: nameController.text,
-                  score: _currentScore,
-                  country: selectedCountry ?? 'No Country Selected',
-                  onUpdate: _onXButtonPressed,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: FutureBuilder<void>(
-                  future: Global.xCountModel.initializeXCount(_selectedSongID ?? 0),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
-                    } else if (snapshot.hasError) {
-                      return Text('Error loading xCount');
-                    } else {
-                      return Consumer<XCountModel>(
-                        builder: (context, xCountModel, child) {
-                          return Text('Votes to Skip: ${xCountModel.xCount}');
-                        },
-                      );
-                    }
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: Stack(
+        children: [
+          _buildContent(),
+          if (_showGlitter)
+            GlitterBox(
+              child: Container(), // Empty container since GlitterBox is an effect overlay
+            ),
+        ],
       ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
@@ -184,4 +142,71 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+
+  Widget _buildContent() {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        NameInputField(
+          controller: nameController,
+          onNameChanged: _onNameChanged,
+        ),
+        const SizedBox(height: 20),
+        const Text('Select Country:'),
+        SongDropdown(
+          onSongSelected: _onSongChanged,
+          songId: _selectedSongID ?? 0,
+          userName: currentUserName ?? '',
+          x_count: Global.xCountModel.xCount, // Use global x_count
+        ),
+        const SizedBox(height: 20),
+        ScoreSlider(onScoreChanged: _onScoreChanged),
+        const SizedBox(height: 20),
+        Center(
+          child: VoteButton(
+            songName: selectedSong ?? 'No Song Selected',
+            songId: _selectedSongID ?? 0,
+            userName: nameController.text,
+            score: _currentScore,
+            country: selectedCountry ?? 'No Country Selected',
+            onUpdate: _onVoteButtonPressed,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Center(
+          child: XButton(
+            songName: selectedSong ?? 'No Song Selected',
+            songId: _selectedSongID ?? 0,
+            userName: nameController.text,
+            score: _currentScore,
+            country: selectedCountry ?? 'No Country Selected',
+            onUpdate: _onXButtonPressed,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Center(
+          child: FutureBuilder<void>(
+            future: Global.xCountModel.initializeXCount(_selectedSongID ?? 0),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error loading xCount');
+              } else {
+                return Consumer<XCountModel>(
+                  builder: (context, xCountModel, child) {
+                    return Text('Votes to Skip: ${xCountModel.xCount}');
+                  },
+                );
+              }
+            },
+          ),
+        ),
+      ],
+    ),
+  );
+}
 }
